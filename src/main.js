c7fc6ce4838b1cb9008538d3d43e32aeb4fa6001
Nishant Gaur <nishantgaur1110@gmail.com>
2025-08-17 16:12:19 +0530

    resolve css file conflict

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import "../style.css";  // go one folder up, because main.js is inside src/
+import "./style.css";  // go one folder up, because main.js is inside src/
 import products from "../api/products.json";  // this is fine
 import { showProductContainer } from "./homeProductCards"; // add ./ because homeProductCards.js is inside src/
  
